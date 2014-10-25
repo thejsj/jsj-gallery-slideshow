99173e2919ea10d14a6116ba7a8da1faf829c3cc
thejsj <jorge.silva@thejsj.com>
2014-10-24 20:09:05 -0700

    Added span and class to divider between Next and previous. Fixes #6

--- themes/default-captions/template.php
+++ themes/default-captions/template.php
@@ -1,5 +1,5 @@
-<div 
-	id='<?php echo $name_space; ?>-gallery-container-<?php echo $id; ?>' 
+<div
+	id='<?php echo $name_space; ?>-gallery-container-<?php echo $id; ?>'
 	class='gallery-container <?php echo $name_space; ?>-gallery-container cycle-slideshow <?php echo $theme_name; ?>'
 	data-numbering-translation-of='<?php echo $number_translation; ?>'
 	data-total='<?php echo $count; ?>'
@@ -8,13 +8,14 @@
 	<!-- Navigation -->
 	<div class='gallery-navigation'>
 		<a id='<?php echo $name_space; ?>-gallery-prev-<?php echo $id; ?>' class='gallery-prev gallery-button <?php echo $prev_class; ?>' href='#'><?php echo $previous_text; ?></a>
-		 / <a id='<?php echo $name_space; ?>-gallery-next-<?php echo $id; ?>' class='gallery-next gallery-button <?php echo $next_class; ?>' href='#'><?php echo $next_text; ?></a> 
+		<span class='divider'>/</span>
+		<a id='<?php echo $name_space; ?>-gallery-next-<?php echo $id; ?>' class='gallery-next gallery-button <?php echo $next_class; ?>' href='#'><?php echo $next_text; ?></a>
 		<span id='<?php echo $name_space; ?>-gallery-numbering-<?php echo $id; ?>' class='gallery-numbering <?php echo $numbering_class; ?>'></span>
 	</div>
 
 	<!-- Start Gallery -->
-	<div 
-		id='<?php echo $name_space; ?>-gallery-<?php echo $id; ?>' 
+	<div
+		id='<?php echo $name_space; ?>-gallery-<?php echo $id; ?>'
 		class='gallery gallery-columns-<?php echo $columns; ?> gallery-size-<?php echo $size_class; ?> <?php echo $name_space; ?>-gallery <?php echo $selector; ?> <?php echo $theme_name; ?>'
 	>
 		<!-- Include all images as HTML -->
